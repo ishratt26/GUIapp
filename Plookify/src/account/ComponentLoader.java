19720598c21517624557a0821015312452e924a5
Alen <Alen@192.168.1.6>
2016-01-14 22:32:04 +0000

    Adding <account> package

---
+++ Plookify/src/account/ComponentLoader.java
@@ -0,0 +1,13 @@
+package account;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alen
+ */
+public class ComponentLoader {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "The application works.");
+    }
+}
