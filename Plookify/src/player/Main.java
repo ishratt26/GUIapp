5f4daaa70bb283756b4dc9db81c5efd0e78eeb4b
Tunazzina Ishrat <t.ishrat@se14.qmul.ac.uk>
2016-02-07 23:22:56 +0000

    adding Main class for player

---
+++ Plookify/src/player/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package player;
+
+import javafx.application.Application;
+
+/**
+ *
+ * @author tunazzinaIshrat
+ */
+public class Main {
+    
+}
