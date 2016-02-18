db64cb5187cce5f8a67c697b004126d4f395f070
Tunazzina Ishrat <t.ishrat@se14.qmul.ac.uk>
2016-02-18 22:21:37 +0000

    fixing

--- Plookify/src/player/Main.java
+++ Plookify/src/player/Main.java
@@ -2,7 +2,6 @@ package player;
 
 import player.gui.MainStage;
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.stage.Stage;
 import player.gui.TrackScene;
 
