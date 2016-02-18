8e7ba050c6231f7c40e66281fa36877783536f4b
Tunazzina Ishrat <t.ishrat@se14.qmul.ac.uk>
2016-02-18 21:55:49 +0000

    fixing

--- Plookify/src/player/Main.java
+++ Plookify/src/player/Main.java
@@ -1,6 +1,6 @@
 package player;
 
-import account.gui.MainStage;
+import player.gui.MainStage;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.stage.Stage;
