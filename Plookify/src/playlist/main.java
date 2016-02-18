559764d9a1eae22014fbf2f8ba0736fa86a0c7ad
Monika Dzhaleva <monicadjaleva@abv.bg>
2016-02-18 17:02:13 +0000

    commiting

--- Plookify/src/playlist/main.java
+++ Plookify/src/playlist/main.java
@@ -1,40 +1,25 @@
 package playlist;
 
+import account.gui.MainStage;
 import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import static javafx.application.Application.launch;
 import javafx.stage.Stage;
-/**
- *
- * @Monika Dzhaleva 
- */
+import playlist.gui.PlaylistScene;
 
 public class main extends Application {
-    
-    
-    
-    @FXML
-    Button NewPlaylist;
-    @Override
-    public void start(Stage stage) throws Exception {
-        
-        Parent root = FXMLLoader.load(getClass().getResource("gui/playlist.fxml"));
-        
-        Scene scene = new Scene(root);
-        stage.setTitle("Playlist");
-        stage.setScene(scene);
-        stage.show();
-    }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
-  
+
+    @Override
+    public void start(Stage stage) {
+        stage = MainStage.getStage();
+        PlaylistScene scene = new PlaylistScene();
+
+        MainStage.loadScene(scene.getScene(), "Playlist");
+        stage.show();
+    }
 }
+
+
