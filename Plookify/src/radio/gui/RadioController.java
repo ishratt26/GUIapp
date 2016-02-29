feb15b72b6aae3cc2572f63a4293ed3343f4bc3d
Gabriel Uliano <gabrieluliano@138.37.230.247>
2016-02-29 16:01:18 +0000

    radio songs

--- Plookify/src/radio/gui/RadioController.java
+++ Plookify/src/radio/gui/RadioController.java
@@ -10,12 +10,10 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.HashSet;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
 import javafx.collections.FXCollections;
 /**
  * FXML Controller class
@@ -26,28 +24,81 @@ public class RadioController {
 
    @FXML private TextField textField;
    @FXML ListView<String> listView;
-  // @FXML ListView<String> artistView;
- //  @FXML ListView<String> artistList;
- //  @FXML ListView<String> genreList;
-   
-   
-   
-   
+   @FXML ListView<String> songView;
+   ArrayList<String> PLAYLIST = new ArrayList<String>();
    public RadioController(){
-   
    }
    
     @FXML protected void searchButton() {
-        String artist = textField.getText();
+        String artist = textField.getText(); //Search text field
+        ObservableList<String> artistData = FXCollections.observableArrayList(); //List for storing artists names
+        Connection c = null;
+        Statement stmt = null;    
+        
+        try
+        {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:plookifyDB.sqlite"); //connecting to database
+            c.setAutoCommit(false);
+            System.out.println("Here: " + c.getCatalog());
+            System.out.println("Opened database successfully"); //verify database opened succesfully 
+            stmt = c.createStatement();
+            
+            ResultSet q1 = stmt.executeQuery( "select artistID from artist where artistName ='"+artist+"';");//get artistID from artist table
+                while ( q1.next() ){
+                    int id = q1.getInt("artistID");
+                    ResultSet q2 = stmt.executeQuery( "select trackGenre from track where trackArtist ="+id+";"); 
+                    int gen = q2.getInt("trackGenre");
+                    ResultSet q2_1 = stmt.executeQuery( "select genreName from genre where genreID ="+gen+";");
+                    while(q2.next()){
+                        String genre = q2_1.getString("genreName");           
+                    
+                }
+            ResultSet q3 = stmt.executeQuery( "select trackArtist from track where trackGenre ="+gen+";");
+                ArrayList<Integer> artistID = new ArrayList<Integer>();
+               
+                while(q3.next()){
+                    int artistNumber = q3.getInt("trackArtist");
+                    if(!artistID.contains(artistNumber))
+                        artistID.add(artistNumber);
+                }
+                
+                System.out.println(artistID);
+                
+            int counter=0;
+            while(counter<4){
+                ResultSet q4 = stmt.executeQuery("select artistName from artist where artistID= "+artistID.get(counter)+";");
+                    while(q4.next()){
+                    String artistName = q4.getString("artistName");
+                    System.out.println(artistName);
+                    counter++;
+                    artistData.addAll(artistName);
+                    }
+            }                  
+            }
+            listView.setItems(artistData);        
+        }
+     
+        catch ( Exception e )
+        {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0); 
+        }
+    }
+    
+    @FXML protected void artistButton() {
+        ObservableList<String> artistData;
+        artistData = listView.getSelectionModel().getSelectedItems();
+        
+        ObservableList<String> songData = FXCollections.observableArrayList();
+        
+        
+       
          
         Connection c = null;
         Statement stmt = null;    
         
-        
-        
-      //  ObservableList<String> nameData = FXCollections.observableArrayList();
-          ObservableList<String> artistData = FXCollections.observableArrayList();
-     //   ObservableList<String> genreData = FXCollections.observableArrayList();
+       
         
         try
         {
@@ -57,77 +108,41 @@ public class RadioController {
             System.out.println("Here: " + c.getCatalog());
             System.out.println("Opened database successfully");
             stmt = c.createStatement();
-            
-            ResultSet q1 = stmt.executeQuery( "select artistID from artist where artistName ='"+artist+"';");//(as if it's typing into terminal)
+            String aName = artistData.get(0);
+            ResultSet q1 = stmt.executeQuery( "select artistID from artist where artistName ='"+aName+"';");//(as if it's typing into terminal)
             while ( q1.next() ) 
-            {
+            {   
                 int id = q1.getInt("artistID");
+                System.out.println(id);
                 ResultSet q2 = stmt.executeQuery( "select trackGenre from track where trackArtist ="+id+";"); 
                 int gen = q2.getInt("trackGenre");
                 ResultSet q2_1 = stmt.executeQuery( "select genreName from genre where genreID ="+gen+";");
-                while(q2.next())
-                {
-                    String genre = q2_1.getString("genreName");
-                 //   for(int i =0; i <10;i++){
-                //    genreData.addAll(genre);}
+                while(q2.next()){
+                    String genre = q2_1.getString("genreName"); 
                 }
           
-                ResultSet q3 = stmt.executeQuery( "select trackArtist from track where trackGenre ="+gen+";");
-                ArrayList<Integer> artistID = new ArrayList<Integer>();
-               
-                while(q3.next())
-                {
-                 int artistNumber = q3.getInt("trackArtist");
-                 if(!artistID.contains(artistNumber)){
-                     artistID.add(artistNumber);
-                     
-                 }
-                // artistID.add(artistNumber);
-                
-                 //System.out.println(artistid);
-                    /*
-                    int artists = q3.getInt("trackArtist");
-                    ResultSet q4 = stmt.executeQuery("select artistName from artist where artistID= "+artists+";");
-                    while(q4.next()){
-                    String nam = q4.getString("artistName");
-                    System.out.println(nam);
-                    }
-                    //nameData.addAll(traks);    */
-                }
-                System.out.println(artistID);
-                int counter=0;
-                while(counter<4){
-                ResultSet q4 = stmt.executeQuery("select artistName from artist where artistID= "+artistID.get(counter)+";");
-                while(q4.next()){
-                String artistName = q4.getString("artistName");
-                System.out.println(artistName);
-                counter++;
-                artistData.addAll(artistName);
-                }
-                }
-               /* ResultSet q3_1 = stmt.executeQuery( "select trackArtist from track where trackGenre ="+gen+";");
-                while(q3_1.next()){
-                    int artistID = q3_1.getInt("trackArtist");
-                    ResultSet q3_2 = stmt.executeQuery( "select artistName from artist where artistID ="+artistID+";");
-                    String aName = q3_2.getString("artistName");
-                    artistData.addAll(aName);
-                }*/
-                
+                ResultSet q3 = stmt.executeQuery( "select trackName from track where trackGenre ="+gen+";");
+                while(q3.next()){
+                    String traks = q3.getString("trackName");
+                    System.out.println(traks);
+                    
+                    songData.addAll(traks); 
+                    PLAYLIST.add(traks);
+                }       
             } 
-            listView.setItems(artistData);
-           // artistView.setItems(artistData);
-           // genreList.setItems(genreData); 
-            
+            System.out.println(songData);
+            songView.setItems(songData);
         }
-      
-        
-        
         
         catch ( Exception e )
         {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0); 
-        }
+        }       
+    }
+    
+    @FXML protected ArrayList<String> playlistButton() {
+        return PLAYLIST;
     }
 }
 
